70bae0b2632abd61b7224939f521e21a6f360372
djay76 <djay76@gmail.com>
2015-09-10 11:23:18 +0200

    maj

--- scripts/essai.js
+++ scripts/essai.js
@@ -1,12 +1,16 @@
 $(document).ready(function(){
 	var grille = document.getElementById('grille');
 	var context = grille.getContext('2d');
+
 	context.beginPath();
-	context.lineTo(0, 50);
-	context.lineTo(50, 50);
-	context.lineTo(50, 0);
-	context.lineTo(0, 0);
+	context.rect(50, 50, 50, 50);
+	context.fillStyle = "red";
 	context.fill();
-	context.closePath();
+	var flag = new Image();
+	flag.onload = function(){
+		context.drawImage(flag,50,50);
+	}
+	flag.src = "http://localhost/demineur/img/flag.png";
+	
 });
 

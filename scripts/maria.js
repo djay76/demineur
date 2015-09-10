7b6e88f5026d1cabdccd897dc0e319746b28ac14
mayacha <maria.chabab@gmail.com>
2015-09-10 13:53:38 +0200

    rien

--- scripts/maria.js
+++ scripts/maria.js
@@ -1,14 +1,18 @@
 $('document').ready(function(){
 
  // create an new instance of a pixi stage
-    var stage = new PIXI.Stage(0x66FF99);
+ var stage = new PIXI.Stage(0x66FF99);
  
-    // create a renderer instance.
-    var renderer = PIXI.autoDetectRenderer(400, 300);
- 
-    // add the renderer view element to the DOM
-    document.body.appendChild(renderer.view);
+// create a renderer instance.
+var renderer = PIXI.autoDetectRenderer(400, 300);
+
+// add the renderer view element to the DOM
+document.body.appendChild(renderer.view);
 
 var container = new PIXI.Container();
-container.addChild(sprite);
+alpha=0.4;
+
+var carre = new PIXI.Rectangle(x, y, 50, 50);
+container.addChild(carre);
+
 });
